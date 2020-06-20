b516ccc4407f5824f1ecbb2330533ad7ab9fa5e0
FacelessLord <skyres21@yandex.ru>
2020-06-20 17:23:08 +0500

    Server responses

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 import time
 from flask import Flask, request
 
+from api.utils import send_image
+
 app = Flask(__name__)
 
 
@@ -38,3 +40,49 @@ def get_category_list():
             "subcategories": [{"nested": False, "id": "a", "name": "DDDD-A"}]
         }
     ]}
+
+
+@app.route('/api/items')
+def get_items_list():
+    args = request.args
+    count = args.get("count", -1)
+
+    return {"items": [
+        {
+            "cardId": 1241,
+            "header": "Red cat with green eyes",
+            # "Рыжий котик с зелёными глазами",
+            "provider": "Mommy-cat",
+            "price": 0,
+            "isStock": True
+        },
+        {
+            "cardId": 1341,
+            "header": "Kitty with blue eyes",  # "Котёнок с голубыми глазами",
+            "provider": "Mommy-cat",
+            "price": 0,
+            "isStock": True
+        },
+        {
+            "cardId": 1251,
+            "header": "Just nice cat",  # "Просто красивый котик",
+            "provider": "Mommy-cat",  # "Мама-кошка",
+            "price": 0,
+            "isStock": True
+        },
+        {
+            "cardId": 6421,
+            "header": "Just nice cat",
+            "provider": "Mommy-cat",
+            "price": 0,
+            "isStock": True
+        },
+    ]}
+
+
+@app.route('/api/images/main')
+def get_main_icon():
+    args = request.args
+    product_id = args.get("id", -1)
+
+    return send_image("cat1.jpg")
---
+++ api/utils.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from flask import send_file
+
+
+def send_image(image_name: str):
+    ext = image_name[image_name.rfind("."):]
+
+    return send_file(image_name, mimetype='image/'+ext)
