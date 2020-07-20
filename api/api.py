68825db9998a99945b3cec09d5a22447f13babac
FacelessLord <skyres21@yandex.ru>
2020-07-20 13:28:10 +0500

    Register API

--- api/api.py
+++ api/api.py
@@ -1,12 +1,13 @@
 import time
 from json import JSONDecoder
 
-# import Mail as Mail
 from flask import Flask, request
+from werkzeug.security import generate_password_hash
 
 import api.controllers.DatabaseController as dbc
 from api.controllers.CategoryController import CategoryController
 from api.controllers.ImageController import ImageController
+from api.controllers.MailController import MailController
 from api.controllers.ProductController import ProductController
 from api.controllers.ProviderController import ProviderController
 
@@ -38,9 +39,39 @@ providers = ProviderController(db)
 categories = CategoryController(db)
 products = ProductController(db)
 images = ImageController(db)
+mailer = MailController(app, db)
 
 
-# mail = Mail(app)
+@app.route('/api/register', methods=['post'])
+def register():
+    username = request.args.get('username')
+    name = request.args.get('name', type=list)
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    if any(filter(lambda u: u.username == username, db.users())):
+        return { "success": False, "reason": "username" }
+    if any(filter(lambda u: u.username == username, db.registrars())):
+        return { "success": False, "reason": "username" }
+    registrar = db.add_user_registrar(name, username, email, generate_password_hash(password))
+    db.commit()
+    link = registrar.create_confirmation_link()
+    html = f"""<p>Для подтверждения регистрации пройдите по ссылке:</p><br><a>{link}</a>"""
+    mailer.send_html_message("Registration confirmation", [email], html)
+    return { "success": True }
+
+
+@app.route('/api/confirmRegister')
+def confirm_register():
+    username = request.args.get('username')
+    token = request.args.get('token')
+
+    registrar = db.get_user_registrar(username=username)
+    if registrar.code == token:
+        db.add_user_from_registrar(registrar)
+        db.commit()
+        return { "success": True }
+    return { "success": False }
 
 
 @app.route('/api/login', methods=['post', 'get'])
@@ -52,12 +83,15 @@ def login():
         password = request.args.get('password')
 
         user = db.get_user_by_name(username)
+        registrar = db.get_user_registrar(username=username)
         if user:
             access_token = user.load(password)
             if access_token:
                 message = "Correct username and password"
             else:
                 message = "Wrong username or password"
+        elif registrar:
+            message = "You need to confirm your registration."
         else:
             message = "Wrong username or password"
 
@@ -67,7 +101,7 @@ def login():
 @app.route('/api/check_auth')
 def check_auth():
     username = request.args.get('username')
-    access_token = request.args.get('access_token', type=int)
+    access_token = request.args.get('access_token', type=str)
     user = db.get_user_by_name(username)
     return { "result": user is not None and user.access_token == access_token }
 
