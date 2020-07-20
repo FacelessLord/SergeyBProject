fe4a47176d5cd26ab5f124b5a634966cefa4df0b
FacelessLord <skyres21@yandex.ru>
2020-07-20 13:27:50 +0500

    Mailing

---
+++ api/controllers/MailController.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from flask_mail import Message, Mail
+
+from api.controllers.Controller import Controller
+from api.controllers.DatabaseController import DatabaseController as dbc
+
+
+class MailController(Controller):
+    def __init__(self, app, db: dbc):
+        super().__init__(db)
+        self.app = app
+        self.mail = Mail(app)
+
+    def send_plaintext_message(self, subject: str, recipients: List[str], text: str):
+        msg = Message(subject, recipients=recipients)
+        msg.body = text
+        self.mail.send(msg)
+
+    def send_html_message(self, subject: str, recipients: List[str], html: str):
+        msg = Message(subject, recipients=recipients)
+        msg.html = html
+        self.mail.send(msg)
