3fdc2946c048b6716120ecfb59100ab202bfec7a
FacelessLord <skyres21@yandex.ru>
2020-07-20 13:27:38 +0500

    User Registrar in db

--- api/controllers/DatabaseController.py
+++ api/controllers/DatabaseController.py
@@ -77,6 +77,18 @@ class Category:
         parent_id = None
 
 
+class UserRegistrar:
+    id = \
+        username = \
+        email = \
+        updated_on = \
+        code = \
+        password_hash = None
+
+    def create_confirmation_link(self):
+        pass
+
+
 # noinspection PyArgumentList,PyGlobalUndefined
 class DatabaseController:
     # noinspection PyRedeclaration
@@ -89,6 +101,7 @@ class DatabaseController:
         global Product
         global Provider
         global Category
+        global UserRegistrar
 
         class User(db.Model):
             __tablename__ = 'users'
@@ -146,6 +159,21 @@ class DatabaseController:
             def __repr__(self):
                 return "<{}:{}>".format(self.id, self.username)
 
+        class UserRegistrar(db.Model):
+            __tablename__ = 'registrars'
+            id = db.Column(db.Integer(), primary_key=True)
+            username = db.Column(db.String(40), nullable=False, unique=True)
+            name = db.Column(db.String(40))
+            surname = db.Column(db.String(40))
+            last_name = db.Column(db.String(40))
+            email = db.Column(db.String(100), nullable=False, unique=True)
+            updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+            code = db.Column(db.String(12), default=guid.new_formatted)
+            password_hash = db.Column(db.String(100), nullable=False)
+
+            def create_confirmation_link(self):
+                return f'/api/confirmRegister?username={self.username}&token={self.code}'
+
         class ProductBatch(db.Model):
             __tablename__ = 'cart'
             id = db.Column(db.Integer(), primary_key=True)
@@ -185,23 +213,39 @@ class DatabaseController:
                  password_hash: str):
         user = User(name=name[0], surname=name[1], last_name=name[2], username=username, email=email, password_hash=password_hash)
         self.db.session.add(user)
+        return user
+
+    def add_user_from_registrar(self, registrar: UserRegistrar):
+        user = User(username=registrar.username, email=registrar.email, password_hash=registrar.password_hash)
+        self.db.session.add(user)
+        return user
+
+    def add_user_registrar(self, name: List[str], username: str, email: str,
+                           password_hash: str):
+        user_registrar = UserRegistrar(name=name[0], surname=name[1], last_name=name[2], username=username, email=email, password_hash=password_hash)
+        self.db.session.add(user_registrar)
+        return user_registrar
 
     def add_product(self, price: float, name: str, provider: int,
                     stocked: bool):
         product = Product(price=price, name=name, provider_id=provider, in_stock=stocked)
         self.db.session.add(product)
+        return product
 
     def add_provider(self, name):
         provider = Provider(name=name)
         self.db.session.add(provider)
+        return provider
 
     def add_item_to_cart(self, customer: int, product: int, amount: float):
         cart_item = ProductBatch(customer_id=customer, product_id=product, amount=amount)
         self.db.session.add(cart_item)
+        return cart_item
 
     def add_category(self, name: str, nested: bool, parent_id: int):
         category = Category(name=name, nested=nested, parent_id=parent_id)
         self.db.session.add(category)
+        return category
 
     def get_category_by_name(self, name: str) -> Category:
         categories = self.db.session.query(Category) \
@@ -236,8 +280,25 @@ class DatabaseController:
         if len(users) > 0:
             return users[0]
 
+    def get_user_registrar(self, username: str = "", email: str = "") -> UserRegistrar:
+        users = None
+        if username:
+            users = self.db.session.query(UserRegistrar) \
+                .filter(UserRegistrar.username == username) \
+                .all()
+        if email:
+            users = self.db.session.query(UserRegistrar) \
+                .filter(UserRegistrar.email == email) \
+                .all()
+
+        if users:
+            return users[0]
+
     def users(self) -> List[User]:
         return self.db.session.query(User)
+
+    def registrars(self) -> List[UserRegistrar]:
+        return self.db.session.query(UserRegistrar)
 
     def get_product(self, product_id: int) -> Product:
         return self.db.session.query(Product) \
