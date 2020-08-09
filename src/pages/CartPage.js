f55a1c6a556912420c10ceceab1e014697f165bd
facelesslord <skyres21@yandex.ru>
2020-08-09 19:54:42 +0500

    Cart page styles fix

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -9,8 +9,8 @@ export function CartPage() {
     return (
         <div id="content_wrapper">
             <div className="main panel">
-                <div className="catalog header">
-                    Ваши товары:
+                <div className="catalog header" style={{marginLeft: "0px"}}>
+                    <h1 style={{marginTop: "12px"}}>Ваши товары:</h1>
                     <CatalogStyleSelector type={"cart"} value={window.user.view} valueSetter={setView}/>
                 </div>
                 <CatalogCart type={window.user.view}/>
